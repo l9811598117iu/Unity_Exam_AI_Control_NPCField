b61f21011a7a5003faadf58dd8fafa3c760bd208
l9811598117iu <55218520+l9811598117iu@users.noreply.github.com>
2019-11-07 10:12:48 +0800

    B1040437_期中

---
+++ UnityProject/Assets/Playercontroller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    [Range(10, 100)]
+    public float speed = 10;
+    [Range(10, 100)]
+    public int jump = 10;
+    Rigidbody r3d;
+    public bool isGround;
+
+    // Use this for initialization
+    void Start()
+    {
+        r3d = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+        Jump();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGround = true;
+        Debug.Log("touch" + collision.gameObject);
+    }
+
+    private void Walk()
+    {
+        r3d.AddForce(new Vector3(speed * Input.GetAxis("Horizontal"), 0,0));
+        r3d.AddForce(new Vector3( 0, 0, speed * Input.GetAxis("Vertical")));
+
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            r3d.AddForce(new Vector3(0, jump,0));
+        }
+
+    }
+}
---
+++ UnityProject/Assets/robotball.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robotball : MonoBehaviour {
+
+	[Header("對話")]
+    public string Say = "安安你好嗎!!!!!!!!!";
+    public float SaySpeed = 0.2f;
+    [Header("任務")]
+    public bool Complete = false;
+    public int BateryFinish = 5;
+    public int Batery = 0;
+
+}
